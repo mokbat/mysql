5aba6d29d37f4144369b1a29f5f68cf4654d9e40
mokbat <mokbat@gmail.com>
2018-02-01 21:34:07 -0500

    Create groupby.sql

---
+++ groupby.sql
@@ -0,0 +1,7 @@
+###################
+# Group Statement #
+###################
+
+# The below statement does a grouping of the data by roll_no for mytest table
+
+SELECT * FROM mytest GROUP BY roll_no;
