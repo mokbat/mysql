144765888f2edde32034fa4df03de941272e812e
mokbat <mokbat@gmail.com>
2018-02-01 21:31:58 -0500

    Create select.sql

---
+++ select.sql
@@ -0,0 +1,9 @@
+####################
+# Select Statement #
+####################
+
+# The below statement would select tables mytest and ssn to display it
+
+SELECT * FROM mytest;
+
+SELECT * FROM ssn;
