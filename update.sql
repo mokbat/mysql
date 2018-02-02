2979986af21ce2d8fdf5c6c2f1c32943869ed6b9
mokbat <mokbat@gmail.com>
2018-02-01 21:27:29 -0500

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,7 +1,7 @@
 # This is an update statement
-```
+
 UPDATE mytest SET first_name='williaam', last_name='jonathan' WHERE roll_no='B';
-```
+
 
 ```
 The above statement updates the first_name to 'williaam' and last_name to 'jonathan' where roll_no is 'B'
