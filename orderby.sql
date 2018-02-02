1655e6dbdc1077e64a74ee9914e13b9fc7f5c4c2
mokbat <mokbat@gmail.com>
2018-02-01 21:33:13 -0500

    Create orderby.sql

---
+++ orderby.sql
@@ -0,0 +1,7 @@
+######################
+# Order By Statement #
+######################
+
+# The below statement does order the data by roll_no for mytest table
+
+SELECT * FROM mytest ORDER BY roll_no;
