65d95a90527873526648eb597559450616e4fd57
mokbat <mokbat@gmail.com>
2018-02-01 21:40:54 -0500

    Create showcolumn.sql

---
+++ showcolumn.sql
@@ -0,0 +1,9 @@
+#####################################
+# Show Column Information Statement #
+#####################################
+
+# The below statement shows the column information on two tables mytest and ssn
+
+show columns from mytest;
+
+show columns from ssn;
